afe812a2a1ad3490aa46269f5441cb7a4dd31deb
wushuai0413 <13265128312@163.com>
2017-12-27 15:04:28 +0800

    增加一个类别

---
+++ GitUseExample/NSObject+MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+MapForItinerary.h
+//  GitUseExample
+//
+//  Created by 伍帅 on 27/12/17.
+//  Copyright © 2017年 伍帅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (MapForItinerary)
+
+@end
---
+++ GitUseExample/NSObject+MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+MapForItinerary.m
+//  GitUseExample
+//
+//  Created by 伍帅 on 27/12/17.
+//  Copyright © 2017年 伍帅. All rights reserved.
+//
+
+#import "NSObject+MapForItinerary.h"
+
+@implementation NSObject (MapForItinerary)
+
+@end
