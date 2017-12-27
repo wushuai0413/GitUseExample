7de978b11f58e4f803ebec7262e611df8d9bd801
wushuai0413 <13265128312@163.com>
2017-12-27 15:32:44 +0800

    增加一个数组类别

---
+++ GitUseExample/NSArray+JsonModelArray.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+JsonModelArray.h
+//  GitUseExample
+//
+//  Created by 伍帅 on 27/12/17.
+//  Copyright © 2017年 伍帅. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (JsonModelArray)
+
+@end
